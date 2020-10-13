3b043b89d57a0ef7593ce88a984da59ce313e3ee
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-13 07:53:31 +0600

    Footer component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { JumbotronContainer } from "./containers/JumbotronContainer";
+import { FooterContainer } from "./containers/FooterContainer";
 
 export default function App () {
     return (
         <>
             <JumbotronContainer />
+            <FooterContainer />
         </>
     );
 }
