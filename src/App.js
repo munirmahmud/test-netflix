222fa49d4a9c39a5856fee1ef6da4554f2534875
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-13 09:37:35 +0600

    Route constants done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { JumbotronContainer } from "./containers/JumbotronContainer";
 import { FooterContainer } from "./containers/FooterContainer";
 import {FaqsContainer} from "./containers/FaqsContainer";
+import * as ROUTES from './constants/routes';
 
 export default function App () {
     return (
