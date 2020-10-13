001181c74c329208fd896dfe73360b57c9e8448f
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-13 09:45:00 +0600

    Route setup for the home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { JumbotronContainer } from "./containers/JumbotronContainer";
 import { FooterContainer } from "./containers/FooterContainer";
-import {FaqsContainer} from "./containers/FaqsContainer";
+import { FaqsContainer } from "./containers/FaqsContainer";
 import * as ROUTES from './constants/routes';
 
 export default function App () {
     return (
-        <>
-            <JumbotronContainer />
-            <FaqsContainer />
-            <FooterContainer />
-        </>
+        <Router>
+            <Route exact path={ROUTES.HOME}>
+                <JumbotronContainer />
+                <FaqsContainer />
+                <FooterContainer />
+            </Route>
+        </Router>
     );
 }
 
