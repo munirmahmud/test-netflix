acebc86467f91a25ba578487199a3cf7857a8a42
Munir Mahmud <munirmahmud6@gmail.com>
2020-10-13 22:50:56 +0600

    Sign in authentication is done

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from 'react-router-dom';
 import { FooterContainer } from "../containers/FooterContainer";
 import { HeaderContainer } from "../containers/HeaderContainer";
 import { Form } from "../components";
+import { FirebaseContext } from "../context/firebase";
+import * as ROUTES from '../constants/routes';
 
 
 export function Signin() {
+    const history = useHistory()
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const isInvalid = emailAddress === '' || password === '';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        firebase.auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => {
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            });
+    }
+
     return (
         <>
             <HeaderContainer>
                 <Form>
                     <Form.Text>Sign In</Form.Text>
+                    {error && <Form.Error>{error}</Form.Error>}
 
-                    <Form.Base method="POST">
-                        <Form.Input type="email" placeholder="Email Address" />
-                        <Form.Input type="password" placeholder="Password" autocomplete="off" />
-                        <Form.Submit type="submit">Sign In</Form.Submit>
+                    <Form.Base onSubmit={handleSubmit} method="POST">
+                        <Form.Input type="email" value={emailAddress} onChange={({target}) => setEmailAddress(target.value)} placeholder="Email Address" />
+                        <Form.Input type="password" value={password} onChange={({target}) => setPassword(target.value)} placeholder="Password" autocomplete="off" />
+                        <Form.Submit type="submit" disabled={isInvalid}>Sign In</Form.Submit>
                     </Form.Base>
 
                     <Form.Text>
